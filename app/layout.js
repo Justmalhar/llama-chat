ef09f1d890240c9b515c2ce4159e8a8be24029f5
Malhar Ujawane <Malharujawane@gmail.com>
2023-11-05 23:44:22 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,10 @@ import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 export const metdata = {
-  title: "Llama Chat",
+  title: "Uncensored Alpine Chat",
   openGraph: {
-    title: "Llama Chat",
-    description: "Chat with Llama 2",
+    title: "Uncensored Alpine Chat",
+    description: "Chat with Uncensored AI",
   },
 };
 
@@ -13,10 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Chat with Llama 2</title>
+        <title>Uncensored Alpine Chat</title>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐬</text></svg>"
         />
       </head>
       <body>
